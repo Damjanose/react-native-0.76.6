f1e04d889cd025b385f5ad29bc1e6c8454912b07
Damjano <damjano@Damjanos-MacBook-Pro.local>
2025-02-11 14:46:15 +0100

    bottom tabs with animation

--- src/navigation/clientRoutes/bottomTabsNavigator/ClientBottomTabNavigator.tsx
+++ src/navigation/clientRoutes/bottomTabsNavigator/ClientBottomTabNavigator.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from '../../../screens/HomeScreen';
 import ReservationScreen from '../../../screens/ReservationScreen.tsx';
 import ProfileScreen from '../../../screens/ProfileScreen';
-import * as Animatable from 'react-native-animatable';
+import Tabbar from './Tabbar';
 
 export type BottomTabNavigatorParamList = {
   Home: undefined;
@@ -16,25 +15,7 @@ const Tab = createBottomTabNavigator<BottomTabNavigatorParamList>();
 
 const ClientBottomTabNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          const iconMapping = {
-            Home: 'home',
-            Reservation: 'plus',
-            Profile: 'account',
-          };
-          const iconName = iconMapping[route.name] || 'help-circle';
-          return (
-            <Animatable.View animation="bounceIn">
-              <MaterialCommunityIcons name={iconName} size={size} color={color} />
-            </Animatable.View>
-          );
-        },
-        tabBarActiveTintColor: 'blue',
-        tabBarInactiveTintColor: 'gray',
-        headerShown: false,
-      })}>
+    <Tab.Navigator screenOptions={{ headerShown: false }} tabBar={props => <Tabbar {...props} />}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Reservation" component={ReservationScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
