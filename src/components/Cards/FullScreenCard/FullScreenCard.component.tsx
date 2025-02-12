2ede56d963bf96f44a709cb901a097f2f37edccd
Damjano <damjano@Damjanos-MacBook-Pro.local>
2025-02-12 17:11:09 +0100

    fix: improve layout of FullScreenCard component

--- src/components/Cards/FullScreenCard/FullScreenCard.component.tsx
+++ src/components/Cards/FullScreenCard/FullScreenCard.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card } from 'react-native-paper';
 import { Props as CardProps } from 'react-native-paper/lib/typescript/components/Card/Card';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 import { fullHeight } from './FullScreenCard.styles';
 import GoBackHeader from '../../GoBackHeader';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -9,23 +9,27 @@ import colors from '../../../styles/colors';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { isIOS } from '../../../utils/device.ts';
 
+const TABBAR_HEIGHT = 60;
+
 type Props = Omit<CardProps & { stickyHeader?: boolean }, 'theme'> & { mode?: 'elevated'; title?: string };
-export const { container } = StyleSheet.create({ container: { height: '100%', borderRadius: 0 } });
+export const { container } = StyleSheet.create({ container: { flex: 1, borderRadius: 0 } });
 export const FullScreenCard = (props: Props) => {
   const insets = useSafeAreaInsets();
   const paddingTop = insets.top > 24 ? (isIOS ? insets.top : insets.top + 12) : 32;
 
   return (
-    <SafeAreaView style={{ backgroundColor: colors.white, paddingTop }}>
-      <KeyboardAwareScrollView
-        stickyHeaderIndices={props.stickyHeader ? [0] : []}
-        bounces={false}
-        nestedScrollEnabled
-        style={fullHeight}
-        showsVerticalScrollIndicator={false}>
-        {props.title && <GoBackHeader title={props.title} />}
-        {props.children}
-      </KeyboardAwareScrollView>
+    <SafeAreaView style={{ backgroundColor: colors.white, paddingTop, flex: 1 }}>
+      <View style={{ flex: 1, paddingBottom: TABBAR_HEIGHT }}>
+        <KeyboardAwareScrollView
+          stickyHeaderIndices={props.stickyHeader ? [0] : []}
+          bounces={false}
+          nestedScrollEnabled
+          style={fullHeight}
+          showsVerticalScrollIndicator={false}>
+          {props.title && <GoBackHeader title={props.title} />}
+          {props.children}
+        </KeyboardAwareScrollView>
+      </View>
     </SafeAreaView>
   );
 };
