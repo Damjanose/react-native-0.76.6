93d1d88b7bea64c18cb7d5ea295cc213657a699c
Damjano <damjano@Damjanos-MacBook-Pro.local>
2025-03-03 14:54:49 +0100

    wip: book taxi

---
+++ src/constants/dimensions.ts
@@ -0,0 +1,7 @@
+import { Dimensions } from 'react-native';
+
+// const { StatusBarManager } = NativeModules;
+
+export const SCREEN_HEIGHT = Dimensions.get('window').height;
+export const SCREEN_WIDTH = Dimensions.get('window').width;
+// export const STATUSBAR_HEIGHT = StatusBarManager.HEIGHT;
--- src/screens/TrackDriverScreen.tsx
+++ src/screens/TrackDriverScreen.tsx
@@ -1,77 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CircularProgress from 'react-native-circular-progress-indicator';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { FullScreenCard } from '../components/Cards/FullScreenCard';
-import colors from '../styles/colors';
+import { Button } from 'react-native-paper';
+import { SCREEN_HEIGHT } from '../constants/dimensions';
 
 const TrackDriverScreen = () => {
-  const [timeLeft, setTimeLeft] = useState<number | null>(5); // Default to 5 minutes for now
-
-  useEffect(() => {
-    const fetchTimer = async () => {
-      try {
-        const response = { estimated_time: 10 }; // Example response (10 minutes)
-        const minutes = response.estimated_time;
-
-        setTimeLeft(minutes * 60); // Convert minutes to seconds
-      } catch (error) {
-        console.error('Error fetching timer:', error);
-      }
-    };
-
-    fetchTimer();
-  }, []);
-
-  useEffect(() => {
-    if (timeLeft === null) {
-      return;
-    }
-
-    const interval = setInterval(() => {
-      setTimeLeft(prev => (prev && prev > 0 ? prev - 1 : 0)); // Decrease every second
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [timeLeft]);
+  const searchClosestTaxi = () => {
+    console.log('a');
+  };
 
   return (
     <FullScreenCard>
-      <View style={styles.timerContainer}>
-        {timeLeft !== null && (
-          <View>
-            <CircularProgress
-              value={(timeLeft / (10 * 60)) * 100}
-              radius={150}
-              activeStrokeWidth={50}
-              inActiveStrokeWidth={50}
-              activeStrokeColor={colors.primary}
-            />
-
-            <Text style={styles.timerText}>{Math.ceil(timeLeft / 60)}</Text>
-            <Text style={styles.timerLabel}>minutes until delivered</Text>
-          </View>
-        )}
+      <View style={styles.container}>
+        <Button textColor="white" style={styles.button} onPress={searchClosestTaxi}>
+          Book a taxi
+        </Button>
       </View>
     </FullScreenCard>
   );
 };
 
 const styles = StyleSheet.create({
-  timerContainer: {
+  container: {
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
     paddingTop: 40,
+    flex: 1,
+    height: SCREEN_HEIGHT,
   },
-  timerText: {
-    fontSize: 48,
-    fontWeight: 'bold',
-    color: colors.green,
-  },
-  timerLabel: {
-    fontSize: 16,
-    color: colors.darkGray,
-    marginTop: 10,
+  button: {
+    backgroundColor: 'purple',
+    borderRadius: 25,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    elevation: 4,
+    width: '40%',
+    alignSelf: 'center',
   },
 });
 
